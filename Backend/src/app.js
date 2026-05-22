959a8e7a2ee6c31e771b6c4659f067caf10ed401
DineshKumarBaghel07 <dineshbaghel6251@gmail.com>
2026-05-22 18:10:42 +0530

    add frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,8 @@ import authRouter from "./routes/auth.routes.js";
 import chatRouter from "./routes/chat.routes.js";
 import morgan from "morgan";
 import cors from "cors";
-
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 
 // Middleware
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.BASE_URL,
     credentials: true,
     methods: [ "GET", "POST", "PUT", "DELETE" ],
 }))
