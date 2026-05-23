df218f2b7ec09e5093051f3d3a8e9fcf80aaadf7
DineshKumarBaghel07 <dineshbaghel6251@gmail.com>
2026-05-23 17:13:47 +0530

    fix mail error

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 import dotenv from 'dotenv'
 dotenv.config();
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         type: 'OAuth2',
         user: process.env.GOOGLE_USER,
