5e211e410edfa62b88662bc6bbb56ba123e9cef0
DineshKumarBaghel07 <dineshbaghel6251@gmail.com>
2026-05-23 19:50:58 +0530

    changes inside mail file

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,34 +1,44 @@
 import nodemailer from "nodemailer";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+
 dotenv.config();
+
 const transporter = nodemailer.createTransport({
-       service: "gmail",
+    service: "gmail",
+
     auth: {
-        type: 'OAuth2',
+        type: "OAuth2",
         user: process.env.GOOGLE_USER,
+        clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFREASH_TOKEN,
-        clientId: process.env.CLIENT_ID
-    },
-    connectionTimeout: 30000,
-        greetingTimeout: 30000
-})
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN
+    }
+});
 
-transporter.verify()
-    .then(() => { console.log("Email transporter is ready to send emails"); })
-    .catch((err) => { console.error("Email transporter verification failed:", err); });
+// REMOVE VERIFY
+// transporter.verify()
 
+export async function sendEmail({
+    to,
+    subject,
+    html,
+    text
+}) {
 
-export async function sendEmail({ to, subject, html, text }) {
+    try {
+        const details =
+            await transporter.sendMail({
+                from: process.env.GOOGLE_USER,
+                to,
+                subject,
+                html,
+                text
+            });
 
-    const mailOptions = {
-        from: process.env.GOOGLE_USER,
-        to,
-        subject,
-        html,
-        text
-    };
+        console.log(details);
 
-    const details = await transporter.sendMail(mailOptions);
-    console.log("Email sent:", details);
+    } catch (err) {
+        console.log("EMAIL ERROR");
+        console.log(err);
+    }
 }
