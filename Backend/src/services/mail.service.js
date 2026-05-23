8193a1f6270a40317c236904b4bb4a4fa5337e84
DineshKumarBaghel07 <dineshbaghel6251@gmail.com>
2026-05-23 16:55:27 +0530

    fix cors error

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 import dotenv from 'dotenv'
 dotenv.config();
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         type: 'OAuth2',
         user: process.env.GOOGLE_USER,
